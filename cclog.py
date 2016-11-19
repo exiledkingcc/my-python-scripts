beb519bb2d416c22157ea91ae394df0746695a17
exiledkingcc <exiledkingcc@gmail.com>
2016-11-19 13:34:04 +0800

    add cclog
    simple colored log output

---
+++ cclog.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+
+import sys
+import logging
+
+
+FORMAT = "%(asctime)s [%(levelname)s] [%(name)s]: %(message)s"
+
+
+class Logger:
+    def __init__(self, name=None):
+        self._logger = logging.getLogger(name)
+
+    def _msg(self, *args):
+        msg = " {}" * len(args)
+        return msg[1:].format(*args)
+
+    def d(self, *args):
+        self._logger.debug(self._msg(*args))
+
+    def i(self, *args):
+        self._logger.info(self._msg(*args))
+
+    def w(self, *args):
+        self._logger.warning(self._msg(*args))
+
+    def e(self, *args):
+        self._logger.error(self._msg(*args))
+
+    def c(self, *args):
+        self._logger.critical(self._msg(*args))
+
+
+class Formatter(logging.Formatter):
+    COLORS = {
+        logging.DEBUG: "\x1b[37m",
+        logging.INFO: "\x1b[32m",
+        logging.WARNING: "\x1b[33m",
+        logging.ERROR: "\x1b[31m",
+        logging.CRITICAL: "\x1b[35m",
+        "default": "\x1b[0m"
+    }
+
+    def __int__(self, fmt=None):
+        logging.Formatter.__init__(self, fmt)
+
+    def format(self, record):
+        result = logging.Formatter.format(self, record)
+        nocolor = Formatter.COLORS["default"]
+        color = Formatter.COLORS.get(record.levelno, nocolor)
+        return color + result + nocolor
+
+
+def init(**kwargs):
+    stream = kwargs.pop("stream", sys.stderr)
+    fmt = kwargs.pop("format", FORMAT)
+    level = kwargs.pop("level", logging.DEBUG)
+
+    if len(kwargs) > 0:
+        k, _ = kwargs.popitem()
+        raise TypeError("invalid keyword argument '{}'".format(k))
+
+    handler = logging.StreamHandler(stream)
+    formatter = Formatter(fmt)
+    handler.setFormatter(formatter)
+    logging.root.addHandler(handler)
+    logging.root.setLevel(level)
+
+
+if __name__ == '__main__':
+    init()
+    log = Logger(__name__)
+    log.d("debug")
+    log.i("info")
+    log.w("warning")
+    log.e("error")
+    log.c("critical")
